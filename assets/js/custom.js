44a4d0f46ebedbc7063a82f08469806c0dc67337
Mugil1410 <mugilaananthan@gmail.com>
2024-07-06 15:14:55 +0530

    reslove font awseom tick issue and updated footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
+    //handle nav bar aauto close after nav bar manu click in mobile and tablet device
     $(".nav-link").click(() => {
         $('.navbar-collapse').collapse('hide');
     });
+
+    //get current copyright year
+    var currentYear = new Date().getFullYear();
+    $('#currentYear').text(currentYear);
 });
